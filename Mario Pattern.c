5c81df321dfce85e67099badec4a00f101ad571a
S.Naveenakrishnan <91528427+NaveenakrishnanS@users.noreply.github.com>
2022-10-13 15:27:26 +0530

    Update Mario Pattern.c

--- Mario Pattern.c
+++ Mario Pattern.c
@@ -3,10 +3,18 @@
 void main(){
     int n,i, j,r,l;
     printf("Height:");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=i;j<=i;j++){
+    scanf("%d",&n); //Height of pyramid
+    for(i=1;i<=n;i++){ // determine the number of rows
+        for(j=i;j<=n;j++){
+            printf(" ");
+        }
+        for(l=1;l<=i;l++){ //left triangle
             printf("*");
         }
+        printf(" "); // vertical line
+        for(r=1;r<=i;r++){ //right triangle
+            printf("*");
+        }
+        printf("\n");
     }
 }
